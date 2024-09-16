8b91aa42909cc3258f45bf66294bbfdf0b52bc2e
tony_gritsenko <anton.grizenko@gmail.com>
2024-09-16 18:31:09 +0300

    fixed routes

--- ui/HomeLink.vue
+++ ui/HomeLink.vue
@@ -1,16 +1,11 @@
 <template>
-  <NuxtLink class="app-header__logo-link" v-if="type === 'qtim'" to="#">
+  <NuxtLink class="app-header__logo-link" to="/">
     <img src="~/assets/images/qtim.svg" alt="Logo" />
   </NuxtLink>
-  <NuxtLink class="app-header__logo-link" v-if="type === 'payot'" to="#">
-    <img src="~/assets/images/payot.svg" alt="Logo" />
-  </NuxtLink>
+
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  type: "qtim" | "payot";
-}>();
 </script>
 
 <style scoped></style>
